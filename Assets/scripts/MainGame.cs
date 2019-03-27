3b92c0a19dab0d377552bfc52428d41ae26160f5
Krisstooph <k@olec.com>
2019-03-27 15:01:20 +0100

    Added 6 shot positions (of axis).

--- Assets/scripts/MainGame.cs
+++ Assets/scripts/MainGame.cs
@@ -21,9 +21,43 @@ public class MainGame : MonoBehaviour
 
     private void CheckInput()
     {
+/*
         if (Input.GetButtonDown("Fire2"))
         {
             ballRb.AddForce(transform.forward * power, ForceMode.Impulse);
         }
+*/
+        if (Input.GetAxis("Horizontal") > 0.0f && Input.GetAxis("Vertical") >= 0.0f)
+        {
+            Debug.Log("Right top corner shot!");
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.0f && Input.GetAxis("Vertical") < 0.0f)
+        {
+            Debug.Log("Right bottom corner shot!");
+        }
+
+        if (Input.GetAxis("Horizontal") < 0.0f && Input.GetAxis("Vertical") >= 0.0f)
+        {
+            Debug.Log("Left top corner shot!");
+        }
+
+        if (Input.GetAxis("Horizontal") < 0.0f && Input.GetAxis("Vertical") < 0.0f)
+        {
+            Debug.Log("Left bottom corner shot!");
+        }
+        
+        if (Input.GetAxis("Horizontal") == 0.0f && Input.GetAxis("Vertical") > 0.0f)
+        {
+            Debug.Log("Center top shot!");
+        }
+
+        if (Input.GetAxis("Horizontal") == 0.0f && Input.GetAxis("Vertical") < 0.0f)
+        {
+            Debug.Log("Center bottom shot!");
+        }
+
+        //Debug.Log("X Axis: " + Input.GetAxis("Horizontal"));
+        //Debug.Log("Y Axis: " + Input.GetAxis("Vertical"));
     }
 }
