7ae4d7a37d00a5905bb36417af249add76f74701
Krisstooph <k@olec.com>
2019-03-28 14:30:17 +0100

    Added InputMgr class and split logic in MainGame. Small refactoring done.

---
+++ Assets/scripts/InputMgr.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputMgr : MonoBehaviour
+{
+    public static bool mantaJoy = true;
+    private static String joy1ButtonShot = "Joy1_Shot";
+    private static String joy1ButtonOk = "Joy1_Ok";
+    private static String joy1ButtonBack = "Joy1_Back";
+    private static String joy1ButtonReset = "Joy1_Reset";
+    private static String joy1Horizontal = "Joy1_Horizontal";
+    private static String joy1Vertical = "Joy1_Vertical";
+    private static String joy2ButtonShot = "Joy2_Shot";
+    private static String joy2ButtonOk = "Joy2_Ok";
+    private static String joy2ButtonBack = "Joy2_Back";
+    private static String joy2ButtonReset = "Joy2_Reset";
+    private static String joy2Horizontal = "Joy2_Horizontal";
+    private static String joy2Vertical = "Joy2_Vertical";
+
+    public static string Joy1Horizontal { get => joy1Horizontal; }
+    public static string Joy1Vertical { get => joy1Vertical; }
+    public static string Joy2Horizontal { get => joy2Horizontal; }
+    public static string Joy2Vertical { get => joy2Vertical; }
+
+    private void Start()
+    {
+        String[] names = Input.GetJoystickNames();
+
+        foreach (String name in names)
+            Debug.Log(name);
+    }
+
+    public static bool IsShotButtonHeld(bool joy1Shooting)
+    {
+        if (joy1Shooting)
+            return Input.GetButton(joy1ButtonShot);
+        else
+            return Input.GetButton(joy2ButtonShot);
+    }
+
+    public static bool IsShotButtonUp(bool joy1Shooting)
+    {
+        if (joy1Shooting)
+            return Input.GetButtonUp(joy1ButtonShot);
+        else
+            return Input.GetButtonUp(joy2ButtonShot);
+    }
+
+    public static bool IsAnyOkButtonDown()
+    {
+        // OK (xbox360) = BACK (manta)
+        return Input.GetButtonDown(joy1ButtonOk) || (!mantaJoy && Input.GetButtonDown(joy2ButtonOk)) || (mantaJoy && Input.GetButtonDown(joy2ButtonBack));
+    }
+
+    public static bool IsAnyBackButtonDown()
+    {
+        // BACK (xbox360) = RESET (manta)
+        return Input.GetButtonDown(joy1ButtonBack) || (!mantaJoy && Input.GetButtonDown(joy2ButtonBack)) || (mantaJoy && Input.GetButtonDown(joy2ButtonReset));
+    }
+
+    public static bool IsAnyResetButtonDown()
+    {
+        // RESET (xbox360) = OK (manta)
+        return Input.GetButtonDown(joy1ButtonReset) || (!mantaJoy && Input.GetButtonDown(joy2ButtonReset)) || (mantaJoy && Input.GetButtonDown(joy2ButtonOk));
+    }
+
+    public static float GetProperVerticalValue(String verticalAxis)
+    {
+        if (mantaJoy && verticalAxis.Equals(joy2Vertical))
+            return -Input.GetAxis(verticalAxis);    // Manta has inverted Y-axis
+
+        return Input.GetAxis(verticalAxis);
+    }
+}
--- Assets/scripts/MainGame.cs
+++ Assets/scripts/MainGame.cs
@@ -3,21 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO:
+// * Correct reset behaviour: ball should not rotate and move
 public class MainGame : MonoBehaviour
 {
-    private static String fireButton = "Fire2";
-    private static String joy1ButtonShot = "Joy1_Shot";
-    private static String joy1ButtonOk = "Joy1_Ok";
-    private static String joy1ButtonBack = "Joy1_Back";
-    private static String joy1ButtonReset = "Joy1_Reset";
-    private static String joy1Horizontal = "Joy1_Horizontal";
-    private static String joy1Vertical = "Joy1_Vertical";
-    private static String joy2ButtonShot = "Joy2_Shot";
-    private static String joy2ButtonOk = "Joy2_Ok";
-    private static String joy2ButtonBack = "Joy2_Back";
-    private static String joy2ButtonReset = "Joy2_Reset";
-    private static String joy2Horizontal = "Joy2_Horizontal";
-    private static String joy2Vertical = "Joy2_Vertical";
     private static float minShotForce = 12.0f;
     private static float maxGoalShotForce = 15.0f;
     private static float maxShotForce = 20.0f;
@@ -25,7 +14,6 @@ public class MainGame : MonoBehaviour
     private static float yShotFactor = 0.43f;
 
     public GameObject ball;
-    public bool mantaJoy = true;
     private Vector3 ballStartPosition;
     private Rigidbody ballRb;
     private float shotForce = minShotForce;
@@ -35,11 +23,6 @@ public class MainGame : MonoBehaviour
     {
         ballRb = ball.GetComponent<Rigidbody>();
         ballStartPosition = ball.gameObject.transform.position;
-
-        String[] names = Input.GetJoystickNames();
-
-        foreach (String name in names)
-            Debug.Log(name);
     }
 
     void Update()
@@ -51,7 +34,7 @@ public class MainGame : MonoBehaviour
     {
         float missedShot = 0.0f;
 
-        if (IsAnyResetButtonDown())
+        if (InputMgr.IsAnyResetButtonDown())
         {
             ball.gameObject.transform.position = ballStartPosition;
             ballRb.velocity = Vector3.zero;
@@ -60,7 +43,7 @@ public class MainGame : MonoBehaviour
             joy1Shooting = !joy1Shooting;
         }
 
-        if (IsShotButtonHeld())
+        if (InputMgr.IsShotButtonHeld(joy1Shooting))
         {
             shotForce += 0.15f;
 
@@ -68,7 +51,7 @@ public class MainGame : MonoBehaviour
                 shotForce = maxShotForce;
         }
 
-        if (IsShotButtonUp())
+        if (InputMgr.IsShotButtonUp(joy1Shooting))
         {
             Vector3 shotVector = transform.forward;
             String footballerHorizontal;
@@ -86,80 +69,40 @@ public class MainGame : MonoBehaviour
 
             if (joy1Shooting)
             {
-                footballerHorizontal = joy1Horizontal;
-                footballerVertical = joy1Vertical;
-                goalkeeperHorizontal = joy2Horizontal;
-                goalkeeperVertical = joy2Vertical;
+                footballerHorizontal = InputMgr.Joy1Horizontal;
+                footballerVertical = InputMgr.Joy1Vertical;
+                goalkeeperHorizontal = InputMgr.Joy2Horizontal;
+                goalkeeperVertical = InputMgr.Joy2Vertical;
             }
             else
             {
-                footballerHorizontal = joy2Horizontal;
-                footballerVertical = joy2Vertical;
-                goalkeeperHorizontal = joy1Horizontal;
-                goalkeeperVertical = joy1Vertical;
+                footballerHorizontal = InputMgr.Joy2Horizontal;
+                footballerVertical = InputMgr.Joy2Vertical;
+                goalkeeperHorizontal = InputMgr.Joy1Horizontal;
+                goalkeeperVertical = InputMgr.Joy1Vertical;
             }
 
-            if (Input.GetAxis(footballerHorizontal) > 0.0f && GetProperVerticalValue(footballerVertical) >= 0.0f)
+            if (Input.GetAxis(footballerHorizontal) > 0.0f && InputMgr.GetProperVerticalValue(footballerVertical) >= 0.0f)
                 shotVector = new Vector3(xShotFactor + missedShot, yShotFactor + missedShot, 1.0f);
 
-            if (Input.GetAxis(footballerHorizontal) > 0.0f && GetProperVerticalValue(footballerVertical) < 0.0f)
+            if (Input.GetAxis(footballerHorizontal) > 0.0f && InputMgr.GetProperVerticalValue(footballerVertical) < 0.0f)
                 shotVector = new Vector3(xShotFactor + missedShot, 0, 1.0f);
 
-            if (Input.GetAxis(footballerHorizontal) < 0.0f && GetProperVerticalValue(footballerVertical) >= 0.0f)
+            if (Input.GetAxis(footballerHorizontal) < 0.0f && InputMgr.GetProperVerticalValue(footballerVertical) >= 0.0f)
                 shotVector = new Vector3(-xShotFactor - missedShot, yShotFactor + missedShot, 1.0f);
 
-            if (Input.GetAxis(footballerHorizontal) < 0.0f && GetProperVerticalValue(footballerVertical) < 0.0f)
+            if (Input.GetAxis(footballerHorizontal) < 0.0f && InputMgr.GetProperVerticalValue(footballerVertical) < 0.0f)
                 shotVector = new Vector3(-xShotFactor - missedShot, 0, 1.0f);
 
-            if (Input.GetAxis(footballerHorizontal) == 0.0f && GetProperVerticalValue(footballerVertical) > 0.0f)
+            if (Input.GetAxis(footballerHorizontal) == 0.0f && InputMgr.GetProperVerticalValue(footballerVertical) > 0.0f)
                 shotVector = new Vector3(0, yShotFactor + missedShot, 1.0f);
 
-            if (Input.GetAxis(footballerHorizontal) == 0.0f && GetProperVerticalValue(footballerVertical) < 0.0f)
+            if (Input.GetAxis(footballerHorizontal) == 0.0f && InputMgr.GetProperVerticalValue(footballerVertical) < 0.0f)
                 shotVector = new Vector3(0, 0, 1.0f);
 
             ballRb.AddForce(shotVector * shotForce, ForceMode.Impulse);
         }
     }
 
-    private bool IsShotButtonHeld()
-    {
-        if (joy1Shooting)
-            return Input.GetButton(joy1ButtonShot);
-        else
-            return Input.GetButton(joy2ButtonShot);
-    }
 
-    private bool IsShotButtonUp()
-    {
-        if (joy1Shooting)
-            return Input.GetButtonUp(joy1ButtonShot);
-        else
-            return Input.GetButtonUp(joy2ButtonShot);
-    }
-
-    private bool IsAnyOkButtonDown()
-    {
-        // MANTA: OK (xbox360) = BACK (manta)
-        return Input.GetButtonDown(joy1ButtonOk) || (!mantaJoy && Input.GetButtonDown(joy2ButtonOk)) || (mantaJoy && Input.GetButtonDown(joy2ButtonBack));
-    }
-
-    private bool IsAnyBackButtonDown()
-    {
-        // MANTA: BACK (xbox360) = RESET (manta)
-        return Input.GetButtonDown(joy1ButtonBack) || (!mantaJoy && Input.GetButtonDown(joy2ButtonBack)) || (mantaJoy && Input.GetButtonDown(joy2ButtonReset));
-    }
-
-    private bool IsAnyResetButtonDown()
-    {
-        // MANTA: RESET (xbox360) = OK (manta)
-        return Input.GetButtonDown(joy1ButtonReset) || (!mantaJoy && Input.GetButtonDown(joy2ButtonReset)) || (mantaJoy && Input.GetButtonDown(joy2ButtonOk));
-    }
-
-    private float GetProperVerticalValue(String verticalAxis)
-    {
-        if (mantaJoy && verticalAxis.Equals(joy2Vertical))
-            return -Input.GetAxis(verticalAxis);    // Manta has inverted Y-axis
-
-        return Input.GetAxis(verticalAxis);
-    }
 }
